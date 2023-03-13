e82a0e9afe4022b61e88f82fef8d577331952ded
aLibraryEnthusiast <alexeason842@gmail.com>
2023-03-13 11:52:15 +0000

    added Discord Stuff for later

--- src/headder_files/state_data.h
+++ src/headder_files/state_data.h
@@ -1,3 +1,7 @@
 #define state_title "title"
 #define state_game "game"
 #define state_pause "pause_game"
+
+#define state_title_DRPC "in the title"
+#define state_game_DRPC "playing %s"
+#define state_pause_DRPC "paused | %s"
